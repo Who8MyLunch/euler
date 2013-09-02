5c1a440f1345d8a60b514f06ea1e07357b9628fd
Pierre V. Villeneuve <pierre.villeneuve@gmail.com>
2013-09-02 11:34:38 -0700

    solved problem 3.

--- problem_003.py
+++ problem_003.py
@@ -21,60 +21,96 @@ Decrement
 
 Check if this number is divisble into the big number.
 
-Only work with odd numbers, except for 5.  Any number ending in five is divisible by 5 and tus not a prime.
+Only work with odd numbers, except for 5.  Any number ending in five is
+divisible by 5 and tus not a prime.
+
+
+
+Two parts to the problem.
+1. Determine if a number is a prime number.
+2. Find the factors of a number.
 
 """
 
 import numpy as np
 
 # Helpers.
-def prime_factor(z):
+def first_factors(z):
     """
-    Search for first prime factor of the input value.
+    Search for first factor of the input value.
     """
+    values_init = [2, 3, 5, 7, 9]
+    for v in values_init:
+        if v >= z:
+            # If we get here then value z must be a prime.
+            return None
 
-    a = int(z**.5)
+        w = z / v
+        if w % 1 == 0:
+            # Found a factor.
+            return v
 
-    for i in [2, 3, 5, 7]:
-        if i >= z:
-            break
+    # Keep searching.
+    value_end = int(z**.5)
+    for i in xrange(10, value_end, 10):
 
-        if z / i % 1 == 0:
-            return i
-
-
-    for i in range(11, a, 10):
         # 11
-        if z / i % 1 == 0:
-            return i
+        v = i + 1
+        w = z / v
+        if w % 1 == 0:
+            return v
 
         # 13
-        if z / (i+2) % 1 == 0:
-            return i
+        v = i + 3
+        w = z / v
+        if w % 1 == 0:
+            return v
 
         # 17
-        if z / (i+6) % 1 == 0:
-            return i
+        v = i + 7
+        w = z / v
+        if w % 1 == 0:
+            return v
 
         # 19
-        if z / (i+8) % 1 == 0:
-            return i
+        v = i + 9
+        w = z / v
+        if w % 1 == 0:
+            return v
 
-    # If we get this far then z must be a prime.
+
     return None
+
+
+
+def find_factors(z):
+    """
+    Search for all factors of input value.
+
+    This is a generator.
+    """
+    while True:
+        a = first_factors(z)
+
+        if a:
+            # print(a)
+            yield a
+            z = int(z / a)
+        else:
+            # print(z)
+            yield z
+            break
 
 #################################################
 
 
 # Setup.
 value = 600851475143
+# value = 8462696833
+# value = 13195
 
 # Do it.
-# while True:
-
-
-
-
-# print('\nThe answer: {:d}'.format(answer))
+for v in find_factors(value):
+    print(v)
 
 # Done.
